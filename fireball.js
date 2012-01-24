b793601e44800c4ef988c0a6e315a38b32c6c8b0
Terry Sutton <saltcod@gmail.com>
2012-01-24 12:39:48 -0330

    reorganized.

---
+++ fireball.js
@@ -0,0 +1,29 @@
+javascript:(function(){
+
+	// the minimum version of jQuery we want
+	var v = "1.3.2";
+
+	// check prior inclusion and version
+	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+		var done = false;
+		var script = document.createElement("script");
+		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+				done = true;
+				initMyBookmarklet();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	} else {
+		initMyBookmarklet();
+	}
+
+	function initMyBookmarklet() {
+		(window.myBookmarklet = function() {
+			// your JavaScript code goes here!
+			document.body.appendChild(document.createElement('script')).src='https://raw.github.com/saltcod/Fireball/master/snowball.js';
+		})();
+	}
+
+})(); 
