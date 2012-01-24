62eca9628c6c6fa301beeb4e2b5762cfd0f18a89
Terry Sutton <saltcod@gmail.com>
2012-01-24 13:45:58 -0330

    link from snowball.js > elements.js

--- snowball.js
+++ snowball.js
@@ -1,1 +1,1 @@
-javascript:(function(){document.body.appendChild(document.createElement("script")).src="https://raw.github.com/saltcod/Fireball/master/snowball.js";document.body.appendChild(document.createElement("script")).src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"})()
+javascript:(function(){document.body.appendChild(document.createElement("script")).src="https://raw.github.com/saltcod/Fireball/master/elements.js";document.body.appendChild(document.createElement("script")).src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"})()
