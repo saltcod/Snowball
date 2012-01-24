ab5bc969b7e1c45e3b4a46a225869f4da3cdbe76
Terry Sutton <saltcod@gmail.com>
2012-01-24 13:40:50 -0330

    Remove cruft from old elements, rename files

---
+++ elements.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('body, .main, .content,#content, #container').css("background", "#F4F3EB");
+	$('*').css("color", "#3D3936");
+	$('.article').css("font-size", "13px");
+	$('.article').css("line-height", "22px");
+	$('#Banner img').css("background-color", "#555E66");
+	$('a:hover').css("color","#F4F3EB")
+	
+});
+
+ 
--- snowball.js
+++ snowball.js
@@ -1,11 +1,1 @@
-$(document).ready(function(){
-	$('body, .main, .content,#content, #container').css("background", "#F4F3EB");
-	$('*').css("color", "#3D3936");
-	$('.article').css("font-size", "13px");
-	$('.article').css("line-height", "22px");
-	$('#Banner img').css("background-color", "#555E66");
-	$('a:hover').css("color","#F4F3EB")
-	
-});
-
- 
+javascript:(function(){document.body.appendChild(document.createElement("script")).src="https://raw.github.com/saltcod/Fireball/master/snowball.js";document.body.appendChild(document.createElement("script")).src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"})()
