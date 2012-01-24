8c76a8e6bcb7bb2e0ef5c366c5aea109a4390ce4
Terry Sutton <saltcod@gmail.com>
2012-01-24 13:06:07 -0330

    Font-size 13px, line-height: 22px

--- snowball.js
+++ snowball.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
 	$('body, .main, .content, #container').css("background", "#FDF6E3");
 	$('*').css("color", "#3D3936");
+	$('.article').css("font-size", "13px");
+	$('.article').css("line-height", "22px");
+	
 });
