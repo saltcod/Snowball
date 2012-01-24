29926132aa8d45fdeea0bf29176b8198c5f54257
Terry Sutton <saltcod@gmail.com>
2012-01-24 12:59:50 -0330

    purple > green

--- snowball.js
+++ snowball.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-	$('body, .main, .content, #container').css("background", "purple");
+	$('body, .main, .content, #container').css("background", "green");
 	$('body, .article, h1, h2, h3, h4').css("color", "#444");
 });
