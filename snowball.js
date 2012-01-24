aa97019776179d5c99c6ba671da43f36a184c8a9
Terry Sutton <saltcod@gmail.com>
2012-01-24 13:07:57 -0330

    A little dimmer on the bg colour.

--- snowball.js
+++ snowball.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('body, .main, .content, #container').css("background", "#FDF6E3");
+	$('body, .main, .content, #container').css("background", "#F4F3EB");
 	$('*').css("color", "#3D3936");
 	$('.article').css("font-size", "13px");
 	$('.article').css("line-height", "22px");
