ab2d5abc98df94a40f888b3f8a94f5205dfaa87b
Terry Sutton <saltcod@gmail.com>
2012-01-24 11:52:36 -0430

    changed yellow > purple

--- snowball.js
+++ snowball.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-	$('body, .main, .content, #container').css("background", "#F4EED9");
+	$('body, .main, .content, #container').css("background", "purple");
 	$('body, .article, h1, h2, h3, h4').css("color", "#444");
 });
