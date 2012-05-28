93b69ccb5e85ec4494e5be35f07ab904dc838bb7
Terry Sutton <saltcod@gmail.com>
2012-05-28 09:57:11 -0230

    more commas

--- elements.js
+++ elements.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
-	$('body, .main, .content,#content, #container').css("background", "#F4F3EB");
-	$('*').css("color", "#3D3936");
-	$('.article').css("font-size", "13px");
-	$('.article').css("line-height", "22px");
+	$('body, .main, .content,#content, #container').css({
+			background: '#F4F3EB',
+			backgroundColor: '#F4F3EB' 
+			});
+	$('*').css({
+			'color': '#3D3936',
+			'text-shadow': 'none'
+			});
+	$('.article').css({
+			'font-size': '13px',
+			'line-height': '22px'
+			});
 	$('#Banner img').css("background-color", "#555E66");
 	$('a:hover').css("color","#F4F3EB");
-	
+	$('pre').css({ 
+			'background': '#EBE9DC', 
+			'border': 'none',
+			'box-shadow': 'none'
+			});
 });
 
- 
