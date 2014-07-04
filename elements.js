65817bbeec381a8c96e2647fe06aeb5fb890d249
Terry Sutton <saltcod@gmail.com>
2014-07-04 14:57:16 -0230

    try getting rid of all backgrounds.

--- elements.js
+++ elements.js
@@ -1,30 +1,34 @@
 $(document).ready(function(){
+	
 	$('html, body, .main, .content,#content, #container,.singular #primary, #branding, #hgroup').css({
-			background: '#F4F3EB',
-			backgroundColor: '#F4F3EB' 
-			});
+		background: '#F4F3EB',
+		backgroundColor: '#F4F3EB' 
+	});
 	$('*').css({
-			'color': '#3D3936',
-			'text-shadow': 'none'
-			});
+		'color': '#3D3936',
+		'text-shadow': 'none',
+		background: '#F4F3EB',
+		backgroundColor: '#F4F3EB'
+	});
+
 	$('.article').css({
-			'font-size': '13px',
-			'line-height': '22px'
-			});
+		'font-size': '13px',
+		'line-height': '22px'
+	});
 	$('#Banner img').css("background-color", "#555E66");
 	$('a:hover').css("color","#F4F3EB");
 	$('pre').css({ 
-			'background': '#EBE9DC', 
-			'border': 'none',
-			'box-shadow': 'none'
-			});
+		'background': '#EBE9DC', 
+		'border': 'none',
+		'box-shadow': 'none'
+	});
 
 	$('blockquote').css({ 
-			'background': 'none', 
-			'color' : '#444',
-			'border-left': '1px solid',
-			'box-shadow': 'none'
-			});
- 
+		'background': 'none', 
+		'color' : '#444',
+		'border-left': '1px solid',
+		'box-shadow': 'none'
+	});
+
 });
 
